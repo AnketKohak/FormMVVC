96628fa25767ff908cd34debb03a873c3c970665
AnketKohak <anketkohak40@gmail.com>
2024-11-14 19:22:12 +0530

    Initial Commit

---
+++ FormMVVC/FormMVVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormMVVCApp.swift
+//  FormMVVC
+//
+//  Created by anket kohak on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FormMVVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
